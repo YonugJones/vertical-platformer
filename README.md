09e36611480e8070b7f86005135ecd5890e3282b
YonugJones <peterckerfoot@gmail.com>
2024-04-19 11:32:54 -0500

    Added gravity component added person class added condition to have player not fall through     ground

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Vertical platformer game following Chris Courses video: 
 https://www.youtube.com/watch?v=rTVoyWu8r6g&ab_channel=ChrisCourses
 
+Using canvas to create a platformer game. 
+This will incorporate gravity, player movement, background movement
--- index.js
+++ index.js
@@ -4,5 +4,47 @@ const ctx = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
-ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+const gravity = 0.5;
+
+class Player{
+    constructor(position) {
+        this.position = position
+        this.velocity = {
+            x: 0,
+            y: 1,
+        }
+        this.height = 100;
+    }
+
+    draw() {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.position.x, this.position.y, 100, this.height);
+    }
+
+    update() {
+        this.draw();
+        this.position.y += this.velocity.y;
+        if (this.position.y + this.height + this.velocity.y < canvas.height) this.velocity.y += gravity
+        else this.velocity.y = 0
+    }
+}
+
+const player = new Player({
+    x: 0,
+    y: 0,
+})
+
+const player2 = new Player({
+    x: 300,
+    y: 100,
+})
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    player2.update();
+}
+
+animate();
